430677eac442afb0d8593b61e9b2816df82f4cab
Flav52 <flavien.riglet52@gmail.com>
2019-05-06 00:16:10 +0200

    Prototype collision serveur

---
+++ php/collision.php
@@ -0,0 +1,40 @@
+<?php
+
+$Map;
+$Joueurs;
+
+//TODO: Ajouter la data 'etat' 'alive' et 'visible' au JSON des joueurs
+
+//Collisions entre joueurs
+for($i = $offset ; $i < sizeof($Joueurs) ; $i++){
+    $current=$Joueurs[i];
+    for($j = $offset ; $j < sizeof($Joueurs) ; $j++){
+        $current2=$Joueurs[j];
+        if($current!=$current2){
+            if($current.position==$current2.position){
+                mange($current,$current2);
+            }
+        }
+    }   
+}
+
+
+function mange($a,$b){
+if($a.etat=='keke' && $b.etat='badger')
+    kill($a);
+else if($b.etat=='keke' && $a.etat='badger')
+    kill($b);
+else if($b.etat=='badger' && $a.etat='badger'){
+    kill($a);
+    kill($b);
+}
+
+function kill($x){
+    for($i=0;i<sizeof($Joueurs);$i++){
+        if($Joueurs[$i].ident==$x.ident){
+            $Joueurs[$i].alive=false;
+        }
+    }
+}
+
+?>
